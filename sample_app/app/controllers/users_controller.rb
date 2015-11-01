50ec1e53275798c65643f1f2382ed709127eec65
moxdev <moxdev1@gmail.com>
2015-11-01 18:33:29 -0500

    finished sessions setup

--- sample_app/app/controllers/users_controller.rb
+++ sample_app/app/controllers/users_controller.rb
@@ -15,6 +15,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
+        log_in @user
         flash[:success] = "Welcome to the party homie"
         redirect_to @user
     else
